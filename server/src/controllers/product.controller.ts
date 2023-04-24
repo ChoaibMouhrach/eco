349ba0c6da2a65aafcceacf9ba1585afa7e1cf19
PACY2 <cmouhrach@gmail.com>
2023-04-24 21:51:31 +0000

    integrating the controller methods and routers to the api

---
+++ server/src/controllers/product.controller.ts
@@ -0,0 +1,19 @@
+import { Response, Request } from "express"
+
+export const index = (_request: Request, _response: Response) => {
+}
+
+export const show = (_request: Request, _response: Response) => {
+
+}
+
+export const store = (_request: Request, _response: Response) => {
+}
+
+export const update = (_request: Request, _response: Response) => {
+
+}
+
+export const destroy = (_request: Request, _response: Response) => {
+
+}
