61b429c7076754d0d5f858c41865686b74d77384
PACY2 <cmouhrach@gmail.com>
2023-05-04 05:12:08 +0000

    Controller implementation

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,142 @@
+import { Request, Response } from "express";
+import {
+  Project,
+  Search,
+  Sort,
+  build,
+  paginate,
+} from "../utils/builder";
+import User from "../models/User";
+import { isValidObjectId } from "mongoose";
+import { BadRequestException, NotFoundException } from "../common";
+import bcrypt from "bcrypt"
+import { config } from "../config/config";
+import { StoreRequest } from "../requests/user/store.request";
+import { UpdateRequest } from "../requests/user/update.request";
+
+export const index = async (
+  request: Request<{}, {}, {}, Record<string, string | undefined>>,
+  response: Response
+) => {
+  const search: Search = {
+    value: request.query.search,
+    trash: request.query.trash,
+    fields: ["firstName", "lastName", "email"],
+  };
+
+  const project: Project = {
+    value: request.query.project,
+    fields: {
+      default: {
+        firstName: true,
+        lastName: true,
+      },
+    },
+  };
+
+  const sort: Sort = {
+    value: request.query.search,
+    fields: ["firstName", "lastName", "email"],
+  };
+
+  const query = build({ sort, search, project, page: request.query.page });
+  let users = await User.aggregate(query);
+
+  users = users.map((user) => {
+    delete user.password;
+    delete user.refreshTokens;
+    delete user.forgotPasswordTokens;
+    delete user.confirmEmailTokens;
+    return user;
+  });
+
+  const response_body = await paginate(
+    users,
+    request.query.page,
+    User,
+    request.query.trash
+  );
+  return response.json(response_body);
+};
+
+export const show = async (request: Request<Record<string, string>>, response: Response) => {
+
+  const { id } = request.params
+
+  if (!isValidObjectId(id)) {
+    throw new BadRequestException("Invalid id")
+  }
+
+  const user = await User.findOne({ _id: id });
+
+  if (!user) {
+    throw new NotFoundException("User Not found")
+  }
+
+  return response.json(user.prepare())
+};
+
+export const store = async (request: StoreRequest, response: Response) => {
+
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    isAdmin,
+    verifiedAt
+  } = request.body
+
+  const user = new User({
+    firstName,
+    lastName,
+    email,
+    password: bcrypt.hashSync(password, Number(config.SALT)),
+    isAdmin,
+    verifiedAt
+  })
+
+  await user.save()
+
+  return response.status(201).json(user.prepare())
+
+};
+
+export const update = async (request: UpdateRequest, response: Response) => {
+
+  const { id } = request.params;
+  const body = request.body
+
+  if (!isValidObjectId(id)) {
+    throw new BadRequestException("Id is invalid")
+  }
+
+  const user = await User.findOneAndUpdate(
+    { _id: id },
+    { $set: body },
+    { new: true }
+  );
+
+  if (!user) {
+    throw new NotFoundException("User does not exists")
+  }
+
+  return response.json(user)
+};
+
+export const destroy = async (request: Request<Record<string, string>>, response: Response) => {
+
+  const { id } = request.params
+
+  if (!isValidObjectId(id)) {
+    throw new BadRequestException("Id is not valid");
+  }
+
+  const user = await User.findOneAndDelete({ _id: id })
+
+  if (!user) {
+    throw new NotFoundException("User not found")
+  }
+
+  return response.sendStatus(204)
+};
---
+++ server/src/requests/user/update.request.ts
@@ -0,0 +1,50 @@
+import { Request } from "express"
+import { Authorize, Validate } from "../../interfaces/Request"
+import { User as IUser } from "../../interfaces/User"
+import { z } from "zod"
+import User from "../../models/User"
+
+export interface UpdateRequest extends Request {
+  body: {
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    password?: string;
+    password_confirmation?: string;
+    isAdmin?: boolean;
+    verifiedAt?: string
+  }
+}
+
+const authorize: Authorize = (user?: IUser) => {
+  return user && user.isAdmin
+}
+
+const validate: Validate = (request: Request) => {
+
+  const schema = z.object({
+    firstName: z.string().min(3).max(60).optional(),
+    lastName: z.string().min(3).max(60).optional(),
+    email: z.string().email().refine(async (email) => {
+      return !(await User.findOne({ email }))
+    }, { message: "User not found" }).optional(),
+    password: z.string().min(8).optional(),
+    password_confirmation: z.string().min(8).optional(),
+    isAdmin: z.boolean().optional(),
+    verifiedAt: z.string().datetime().optional()
+  })
+    .refine(({ password, password_confirmation }) => {
+      if (password || password_confirmation) {
+        return password === password_confirmation
+      }
+      return true
+    }, { message: "Password and password_confirmation does not match" })
+
+  return schema.safeParseAsync(request.body)
+
+}
+
+export default {
+  authorize,
+  validate
+}
