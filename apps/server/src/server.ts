e28ec54c19b15299a6592724123d318f33636202
PACY2 <cmouhrach@gmail.com>
2023-06-08 08:44:37 +0000

    docker production setup

--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,1 +1,13 @@
-console.log("HELLO WORLD");
+import express, { Request, Response } from "express";
+
+const app = express();
+
+app.get("/products", (_req: Request, res: Response) =>
+  res.json([
+    {
+      name: "SAMSUNG S21",
+    },
+  ])
+);
+
+app.listen(3000, () => console.log("The server os listening on port 3000"));
