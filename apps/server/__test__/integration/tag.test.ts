6782fb9e099d02212f7148ea7d63d91d60173798
PACY2 <cmouhrach@gmail.com>
2023-06-14 23:10:16 +0000

    testing for tags and units

---
+++ apps/server/__test__/integration/tag.test.ts
@@ -0,0 +1,275 @@
+import { User } from "@prisma/client";
+import config from "@src/config/config";
+import db from "@src/config/db";
+import { ROLES } from "@src/constants";
+import jwt from "jsonwebtoken";
+import { makeTag, makeUser } from "../config/test-data";
+import request from "../config/request";
+
+let user: User;
+let userAccessToken: string;
+
+let admin: User;
+let adminAccessToken: string;
+
+beforeAll(async () => {
+  user = await db.user.create({
+    data: makeUser(),
+  });
+
+  admin = await db.user.create({
+    data: makeUser(ROLES.ADMIN),
+  });
+
+  userAccessToken = jwt.sign({ id: user.id }, config.SECRET_ACCESS);
+  adminAccessToken = jwt.sign({ id: admin.id }, config.SECRET_ACCESS);
+});
+
+afterAll(async () => {
+  await db.user.deleteMany({
+    where: {
+      OR: [{ id: user.id }, { id: admin.id }],
+    },
+  });
+});
+
+describe("GET /tags", () => {
+  it("Should return 200 with paginated body", async () => {
+    const response = await request().get("/api/products");
+    expect(response.status).toBe(200);
+    expect(response.body.data).toBeDefined();
+    expect(response.body.limit).toBeDefined();
+    expect(response.body.count).toBeDefined();
+    expect(response.body.page).toBeDefined();
+  });
+
+  it("Should return tags", async () => {
+    const response = await request().get("/api/tags");
+    expect(response.status).toBe(200);
+  });
+
+  it("Should return tags with v in in the tag name", async () => {
+    const response = await request().get("/api/tags?search=v");
+    expect(response.status).toBe(200);
+    expect(
+      response.body.data.every((item: { name: string }) =>
+        item.name.includes("v")
+      )
+    ).toBe(true);
+  });
+});
+
+describe("POST /tags", () => {
+  const storeTag = () =>
+    request()
+      .post("/api/tags")
+      .set("Cookie", `accessToken=${adminAccessToken}`);
+
+  it("Should return 201 with the tag", async () => {
+    const tag = makeTag();
+    const response = await storeTag().send(tag);
+
+    expect(response.status).toBe(201);
+    expect(
+      await db.tag.findUnique({
+        where: {
+          name: tag.name,
+        },
+      })
+    ).toBeTruthy();
+
+    await db.tag.delete({
+      where: {
+        name: tag.name,
+      },
+    });
+  });
+
+  it("Should return 400 with name required", async () => {
+    const response = await storeTag();
+    expect(response.status).toBe(400);
+    expect(response.body.content).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          message: "Required",
+          path: ["name"],
+        }),
+      ])
+    );
+  });
+
+  it("Should return 400 with tag already exists", async () => {
+    // generates a random { name : random }
+    const tag = makeTag();
+
+    // store tag
+    await db.tag.create({
+      data: {
+        name: tag.name,
+      },
+    });
+
+    // send http store request
+    const response = await storeTag().send(tag);
+
+    expect(response.status).toBe(400);
+    expect(response.body.content).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          path: ["name"],
+          message: "Tag already exists",
+        }),
+      ])
+    );
+
+    await db.tag.delete({
+      where: {
+        name: tag.name,
+      },
+    });
+  });
+});
+
+describe("UPDATE /tags/:id", () => {
+  const updateTag = (id: number) =>
+    request()
+      .patch(`/api/tags/${id}`)
+      .set("Cookie", `accessToken=${adminAccessToken}`);
+
+  it("Should return 204", async () => {
+    const tagPayload = makeTag();
+
+    const tag = await db.tag.create({
+      data: tagPayload,
+    });
+
+    const newName = `cam${Math.random()}`;
+
+    const response = await updateTag(tag.id).send({
+      name: newName,
+    });
+
+    expect(response.status).toBe(204);
+
+    expect(
+      (
+        await db.tag.findUnique({
+          where: {
+            id: tag.id,
+          },
+        })
+      )?.name
+    ).toBe(newName);
+
+    await db.tag.delete({
+      where: {
+        id: tag.id,
+      },
+    });
+  });
+
+  it("Should return 400 with name required", async () => {
+    const tagPaylaod = makeTag();
+
+    const tag = await db.tag.create({
+      data: tagPaylaod,
+    });
+
+    const response = await updateTag(tag.id);
+
+    expect(response.status).toBe(400);
+    expect(response.body.content).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          message: "Required",
+          path: ["name"],
+        }),
+      ])
+    );
+
+    await db.tag.delete({
+      where: {
+        id: tag.id,
+      },
+    });
+  });
+
+  it("Should return 401 with tag already exists", async () => {
+    const tagPayload = makeTag();
+
+    const tag = await db.tag.create({
+      data: tagPayload,
+    });
+
+    const response = await updateTag(tag.id).send({
+      name: tag.name,
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body.content).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          message: "Tag already exists",
+          path: ["name"],
+        }),
+      ])
+    );
+
+    await db.tag.delete({
+      where: {
+        id: tag.id,
+      },
+    });
+  });
+});
+
+describe("DELETE /tags/:id", () => {
+  const deleteTag = (id: number) =>
+    request()
+      .delete(`/api/tags/${id}`)
+      .set("Cookie", `accessToken=${adminAccessToken}`);
+
+  it("Should return 204", async () => {
+    const tagPayload = makeTag();
+    const tag = await db.tag.create({
+      data: tagPayload,
+    });
+
+    const response = await deleteTag(tag.id);
+
+    expect(response.status).toBe(204);
+  });
+
+  it("Should return 400 with tag not found", async () => {
+    const response = await deleteTag(Math.random());
+
+    expect(response.status).toBe(400);
+    expect(response.body.content).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          message: "Invalid",
+          path: ["xId"],
+        }),
+      ])
+    );
+  });
+});
+
+describe("AUTHORIZATION", () => {
+  it("Should return 401 when user is not admin", async () => {
+    let response = await request()
+      .post("/api/tags")
+      .set("Cookie", `accessToken=${userAccessToken}`);
+    expect(response.status).toBe(401);
+
+    response = await request()
+      .patch("/api/tags/xxx")
+      .set("Cookie", `accessToken=${userAccessToken}`);
+    expect(response.status).toBe(401);
+
+    response = await request()
+      .delete("/api/tags/xxx")
+      .set("Cookie", `accessToken=${userAccessToken}`);
+    expect(response.status).toBe(401);
+  });
+});
