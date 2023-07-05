050651303c3441c81583fa0fe6ed337e2dcc02ff
PACY2 <cmouhrach@gmail.com>
2023-07-05 05:16:28 +0000

    refactoring code

--- apps/client/src/components/ui/form.tsx
+++ apps/client/src/components/ui/form.tsx
@@ -26,18 +26,16 @@ const FormFieldContext = React.createContext<FormFieldContextValue>(
   {} as FormFieldContextValue
 );
 
-const FormField = <
+function FormField<
   TFieldValues extends FieldValues = FieldValues,
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
->({
-  ...props
-}: ControllerProps<TFieldValues, TName>) => {
+>({ ...props }: ControllerProps<TFieldValues, TName>) {
   return (
     <FormFieldContext.Provider value={{ name: props.name }}>
       <Controller {...props} />
     </FormFieldContext.Provider>
   );
-};
+}
 
 const useFormField = () => {
   const fieldContext = React.useContext(FormFieldContext);
